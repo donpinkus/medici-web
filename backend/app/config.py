328b48e87cd79284c4fa38129c863c73054ea190
Don Pinkus <don.pinkus@gmail.com>
2024-03-13 20:03:20 -0700

    remove backend

--- backend/app/config.py
+++
@@ -1,6 +0,0 @@
-import secrets
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-    SECRET_KEY = secrets.token_hex(16)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/app/models.py
+++
@@ -1,6 +0,0 @@
-from . import db
-
-class Drawing(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(128), nullable=False)  # Assuming Firebase user ID is a string
-    drawing_data = db.Column(db.Text, nullable=False)
